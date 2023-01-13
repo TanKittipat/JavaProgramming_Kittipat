bdae69d41f6333a7e04f6e5a8ed5cd86fa0a502d
TanKittipat <kittipat12tan@gmail.com>
2023-01-13 11:26:02 +0700

    13/01/66

---
+++ Demo1/Ascii_test.java
@@ -0,0 +1,7 @@
+public class Ascii_test {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println("ASCII of \"" + c + "\" is\n " + (int) c);
+        System.out.println("Convert to Binary number " + Integer.toBinaryString((int) c));
+    }
+}
